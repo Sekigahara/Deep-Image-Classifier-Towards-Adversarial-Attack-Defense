b870930220305810798fb38b85acc74e0b5bd68d
Andro Aprila <androaprila12@gmail.com>
2023-03-09 10:31:57 +0700

    Utilities script for validation and notebook to training shallow CNN classifier

--- utilities/script/image_utils.py
+++ utilities/script/image_utils.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 
-def image_loader(image_path):
+def image_loader(image_path, resize=(64, 64)):
     if image_path.split("/")[-1].split(".")[1].lower() == 'png':
         image = Image.open(image_path).convert('RGB')
         image = np.array(image)
@@ -12,8 +12,22 @@ def image_loader(image_path):
         image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    return np.divide(image, 255)
+    return np.divide(cv2.resize(image, resize), 255)
 
 def show_image(image, label):
     plt.imshow(image)
     print("Corresponding label : {}".format(label))
+
+def show_image_number(image, label):
+    label_format = np.load("utilities/label_format.npz")['label']
+    print(label_format)
+
+    plt.imshow(image)
+    print("Corresponding label : {}".format(label_format[label]))
+
+
+def load_numpy_dataset(train_path, test_path, k):
+    x_train, y_train = train_path["x"], train_path["y"]
+    x_test, y_test = test_path["x"], test_path["y"]
+
+    return (x_train, y_train), (x_test, y_test)
---
+++ utilities/script/validation_utils.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import confusion_matrix, classification_report
+
+def get_training_plot(history, metric='accuracy', save=False, savepath="weight/"):
+    if metric == 'loss':
+        fig = plt.gcf()
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+    elif metric == 'accuracy' or metric == 'acc':
+        fig = plt.gcf()
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.xlabel('epoch')
+        plt.ylabel('accuracy')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+
+    if save == True and (metric =='accuracy' or metric=='accuracy'):
+        fig.savefig(savepath)
+    elif save == True and metric=='loss':
+        fig.savefig(savepath)
+
+def get_confusion_matrix(y_true, y_pred, save=False, savepath="weight/"):
+    print('\nConfusion Matrix\n')
+    label_format = np.load("utilities/label_format.npz")['label']
+    cm_result = confusion_matrix(y_true=y_true, y_pred=y_pred)
+    #print(cm_result)
+
+    df_cm = pd.DataFrame(cm_result, label_format, label_format)
+    sns.set(font_scale=1.4)
+    sns.heatmap(df_cm, annot=True, annot_kws={"size": 12}, fmt='g')
+    plt.show()
+
+    print(df_cm)
+
+    if save:
+        plt.savefig(savepath)
+
+def get_classification_report(y_true, y_pred):
+    label_format = np.load("utilities/label_format.npz")['label']
+    print('\n\nClassification Report\n')
+    print(classification_report(y_true=y_true, y_pred=y_pred, target_names=label_format))
+
+def save_model(model, savepath="weight/", filename='model.h5'):
+    model.save(savepath + filename)
